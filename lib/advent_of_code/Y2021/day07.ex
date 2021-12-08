a18166b91df0cb48e83b4eba1d866f86ef8625ed
Chronoes <marten@tarkin.ee>
2021-12-08 09:23:38 +0200

    Improved Day 07 Year 2021 solution

--- lib/advent_of_code/Y2021/day07.ex
+++ lib/advent_of_code/Y2021/day07.ex
@@ -7,32 +7,32 @@ defmodule AdventOfCode.Y2021.Day07 do
 
   @spec prep_input(any) :: any
   def prep_input(input) do
-    input
-    |> String.trim()
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    input |> String.trim() |> String.split(",") |> Enum.map(&String.to_integer/1) |> Enum.sort()
   end
 
   @impl true
   @spec solve_first(any) :: any
   def solve_first(input) do
     crabs = prep_input(input)
-    max_fuel = Enum.max(crabs)
+    median = median_value(crabs)
 
-    Enum.map(0..max_fuel, fn pos ->
-      Enum.map(crabs, fn crab -> abs(crab - pos) end)
-      |> Enum.sum()
+    Enum.map(crabs, fn crab ->
+      abs(crab - median)
     end)
-    |> Enum.min()
+    |> Enum.sum()
+  end
+
+  def median_value(list) do
+    Enum.at(list, trunc(length(list) / 2))
   end
 
   @impl true
   @spec solve_second(any) :: any
   def solve_second(input) do
     crabs = prep_input(input)
-    max_fuel = Enum.max(crabs)
+    median = median_value(crabs)
 
-    Enum.map(0..max_fuel, fn pos ->
+    Enum.map(median..length(crabs), fn pos ->
       Enum.map(crabs, fn crab ->
         moved = abs(crab - pos)
         trunc(moved * (1 + (moved - 1) / 2))
